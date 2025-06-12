71bac07571067c50199038dee949ba6092c86872
BiomapWendell <wanghao@biomap.com>
2025-06-12 17:43:29 +0800

    Update esm2_pretrain.sh
    Signed-off-by: BiomapWendell <wanghao@biomap.com>

--- scripts/esm2_pretrain.sh
+++ scripts/esm2_pretrain.sh
@@ -2,6 +2,7 @@
 # This is a demo run
 # before running this script, make sure bionemo has been installed, see ../sub-packages
 # for BioMapper, use the docker image: docker pull harbor.biomap-int.com/wanghao/bionemo:2.6
+# for other users, use nvcr.io/nvidia/clara/bionemo-framework:2.6
 DATA_DIR=$(download_bionemo_data esm2/testdata_esm2_pretrain:2.0 --source ngc)
 
 train_esm2 \
